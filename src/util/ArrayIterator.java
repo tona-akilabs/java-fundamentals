fa2668cbeffce7aba14345651a92a4ade1841f6e
Tona <tona.chheun@akilabs.co>
2025-07-14 10:58:59 +0700

    add

--- src/util/ArrayIterator.java
+++ src/util/ArrayIterator.java
@@ -1,4 +1,31 @@
 package util;
 
-public class ArrayIterator {
+public class ArrayIterator<T> implements Iterable<T> {
+    private final T[] data;
+
+    public ArrayIterator(T[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public java.util.Iterator<T> iterator() {
+        return new java.util.Iterator<>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < data.length;
+            }
+
+            @Override
+            public T next() {
+                return data[index++];
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException("remove");
+            }
+        };
+    }
 }
